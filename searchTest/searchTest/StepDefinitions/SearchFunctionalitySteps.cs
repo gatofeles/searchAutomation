37cc281f47bda355de0581f0821a0ebbe6375215
Wagner Martins Jr <Wagner.Martins@wexinc.com>
2021-07-20 10:38:18 -0300

    bg

--- searchTest/searchTest/StepDefinitions/SearchFunctionalitySteps.cs
+++ searchTest/searchTest/StepDefinitions/SearchFunctionalitySteps.cs
@@ -29,7 +29,7 @@ namespace searchTest.StepDefinitions
         {
             //setting the screen to 1200 x 754 px
             string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string webdriver = sCurrentDirectory + @"\webdriver";
+            string webdriver = sCurrentDirectory + "webdriver";
             chromeOptions.AddArgument("--window-size=1200,754");
             webDriver = new ChromeDriver(webdriver,chromeOptions);
             webDriver.Navigate().GoToUrl("https://www.wexinc.com/");
